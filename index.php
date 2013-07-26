6227086e6f66783542528b42aa7e53793c331df6
Valentin Rusiechi <valentin.rusiechi@gmail.com>
2013-07-26 15:05:41 -0300

    Inclusion de elementos adicionales

--- index.php
+++ index.php
@@ -13,14 +13,9 @@
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script type="text/javascript" src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
         <script src="//api.tucanotours.com.ar/bs/js/bootstrap.min.js"></script>
-        <script>
-            $(function() {
-                $( ".draggable" ).draggable();
-            });
-        </script>
         <!-- GCHARTS INIT -->
-        <script type="text/javascript">google.load('visualization', '1.1', {packages: ['corechart', 'controls']});</script>
         <script type="text/javascript" src="js/reporting-graphs.js"></script>
+        <script type="text/javascript" src="js/data-handler.js"></script>
         <!-- GCHARTS END -->
     </head>
     <body>
@@ -32,7 +27,110 @@
             </div>
         </div>
         <div id="wrapper">
-            <button id="chart" class="btn">Chartttttt</button>
+            <!-- <button id="chart" class="btn">Chartttttt</button> -->
+            <div class="row-fluid">
+                <!--dashboard5-->
+                <div id="dashboard5" class="span6 dashboard draggable">
+                    <div class="btn-group">
+                        <button class="btn btn-mini btn-inverse type" objdata="">Gráfico</button>
+                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu grafico">
+                            <li><a objdata="line">Líneas</a></li>
+                            <li><a objdata="col">Barras</a></li>
+                            <li><a objdata="pie">Torta</a></li>
+                        </ul>
+                    </div>
+                    <div class="btn-group">
+                        <button class="btn btn-mini btn-inverse campo" objdata="">Campo</button>
+                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a objdata="sine">Agente</a></li>
+                            <li><a objdata="cia">Compañía</a></li>
+                            <li><a objdata="pcc">PCC</a></li>
+                        </ul>
+                    </div>
+                    
+                    <div class="btn-group">
+                        <button class="btn btn-mini btn-success sine" objdata="sine">Elija Agente</button>
+                        <button class="btn btn-mini btn-success dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
+                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
+                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
+                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="btn-group">
+                        <button class="btn btn-mini btn-inverse dimension" objdata="">Dimensión</button>
+                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a objdata="pcc">PCC</a></li>
+                            <li><a objdata="limit">Límite</a></li>
+                            <li><a objdata="gds">GDS</a></li>
+                            <li><a objdata="sine">Agente</a></li>
+                        </ul>
+                    </div>
+                    <input objdata="text" class="input-mini limit" placeholder="limite">
+                    <div class="btn-group">
+                        <button class="btn btn-mini btn-inverse filtro" objdata="">Filtro</button>
+                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a objdata="month">Mes</a></li>
+                            <li><a objdata="year">Año</a></li>
+                            <li><a objdata="gds">GDS</a></li>
+                        </ul>
+                    </div>
+                    <!-- 
+                    <div class="drag">
+                        <i class="icon-move" title="Arrastrar gráfico"></i>
+                    </div>
+                    -->
+                    <div>
+                        <div id="piecolchart5" class="pie"></div>
+                    </div>
+                    <div class="graficar">
+                        <button class="btn btn-mini">Graficar</button>
+                    </div>
+                </div>
+                <!--dashboard5-->
+                <!--dashboard4-->
+                <div id="dashboard4" class="span6 dashboard draggable">
+                    <div class="btn-group">
+                        <button class="btn btn-inverse">Tipo de gráfico</button>
+                        <button class="btn btn-inverse dropdown-toggle" data-toggle="dropdown">
+                        <span class="caret"></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Líneas</a></li>
+                            <li><a href="#">Barras</a></li>
+                            <li><a href="#">Torta</a></li>
+                        </ul>
+                    </div>
+                    <div class="graficar">
+                        <button class="btn btn-inverse">Graficar</button>
+                    </div>
+                    <div class="drag">
+                        <i class="icon-move" title="Arrastrar gráfico"></i>
+                    </div>
+                    <div>
+                        <div id="piecolchart4" class="pie"></div>
+                    </div>
+                </div>
+                <!--dashboard4-->
+                
+            </div><!--row-fluid-->
+            <!--dashboard1-->
             <div id="dashboard1" class="span12 dashboard draggable">
                 <div class="btn-group">
                     <button class="btn btn-inverse">Tipo de gráfico</button>
@@ -49,7 +147,7 @@
                     <input type="text" placeholder="Criterio de búsqueda"></input>
                 </div>
                 <div class="graficar">
-                    <button class="btn btn-inverse">Graficar</button>
+                    <button class="btn btn-inverse draw">Graficar</button>
                 </div>
                 <div class="drag">
                     <i class="icon-move" title="Arrastrar gráfico"></i>
@@ -58,7 +156,9 @@
                     <div id="chart1" class="line"></div>
                     <div id="control1" class="line_small"></div>
                 </div>
-            </div><!--dashboard1-->
+            </div>
+            <!--dashboard1-->
+            <!--dashboard2-->
             <div id="dashboard2" class="span12 dashboard draggable">
                 <div class="btn-group">
                     <button class="btn btn-inverse">Tipo de gráfico</button>
@@ -111,52 +211,7 @@
                     <div id="control3" class="line_small"></div>
                 </div>
             </div><!--dashboard3-->
-            <div class="row-fluid">
-                <div id="dashboard4" class="span6 dashboard draggable">
-                    <div class="btn-group">
-                        <button class="btn btn-inverse">Tipo de gráfico</button>
-                        <button class="btn btn-inverse dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Líneas</a></li>
-                            <li><a href="#">Barras</a></li>
-                            <li><a href="#">Torta</a></li>
-                        </ul>
-                    </div>
-                    <div class="graficar">
-                        <button class="btn btn-inverse">Graficar</button>
-                    </div>
-                    <div class="drag">
-                        <i class="icon-move" title="Arrastrar gráfico"></i>
-                    </div>
-                    <div>
-                        <div id="piecolchart4" class="pie"></div>
-                    </div>
-                </div><!--dashboard4-->
-                <div id="dashboard5" class="span6 dashboard draggable">
-                    <div class="btn-group">
-                        <button class="btn btn-inverse">Tipo de gráfico</button>
-                        <button class="btn btn-inverse dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Líneas</a></li>
-                            <li><a href="#">Barras</a></li>
-                            <li><a href="#">Torta</a></li>
-                        </ul>
-                    </div>
-                    <div class="graficar">
-                        <button class="btn btn-inverse">Graficar</button>
-                    </div>
-                    <div class="drag">
-                        <i class="icon-move" title="Arrastrar gráfico"></i>
-                    </div>
-                    <div>
-                        <div id="piecolchart5" class="pie"></div>
-                    </div>
-                </div><!--dashboard5-->
-            </div><!--row-fluid-->
+
         </div><!--wrapper-->
     </body>
 </html>
---
+++ js/data-handler.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var clientObject    = {};
+    var clientDashboard = {};
+    var clientGraph     = {};
+    var that, txt, objdata, value_sabre, value_amadeus;
+
+    $('.dropdown-menu li a').on('click', function(){
+        clientDashboard = $(this).parent().parent().parent().parent();
+        fieldContainer = $(this).parent().parent().parent();
+        that = $(this);
+        txt = that.html();
+        objdata = that.attr('objdata');
+
+        if (objdata == 'sine') {
+            value_sabre = that.attr('value-sabre');
+            value_amadeus = that.attr('value-amadeus');
+            fieldContainer.children('.btn:first').attr('objdata',objdata)
+                                                 .attr('value-amadeus',value_amadeus)
+                                                 .attr('value-sabre',value_sabre)
+                                                 .html(txt);
+        } else {
+            fieldContainer.children('.btn:first').attr('objdata',objdata).html(txt);
+        }
+    });
+
+/*
+    $('.graficar').on('click', function(){
+        clientObject.graph         = clientDashboard.find('.type').attr('objdata');
+        clientObject.id            = clientDashboard.attr('id').slice(-1);
+        clientObject.dimension     = clientDashboard.find('.campo').attr('objdata');
+        clientObject.value         = clientDashboard.find('.value').attr('objdata');
+        clientObject.value_amadeus = clientDashboard.find('.value_amadeus').attr('objdata');
+        clientObject.value_sabre   = clientDashboard.find('.value_sabre').attr('objdata');
+        clientObject.limit         = clientDashboard.find('.limit').attr('objdata');
+
+
+        that = $(this);
+    });
+*/
+
+
+});
