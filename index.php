3af535836aa62371957ca4d00c632317b9779bd3
Valentin Rusiechi <valentin.rusiechi@gmail.com>
2013-07-29 10:12:28 -0300

    fix merge

--- index.php
+++ index.php
@@ -29,8 +29,8 @@
         <div id="wrapper">
             <!-- <button id="chart" class="btn">Chartttttt</button> -->
             <div class="row-fluid">
-                <div id="dashboard5" class="span6 dashboard draggable">
-<<<<<<< HEAD
+                <!--dashboard5-->
+                <div id="dashboard4" class="span6 dashboard draggable">
                     <div class="row1">
                         <div class="btn-group">
                             <button class="btn btn-mini btn-inverse campo" objdata="">Campo</button>
@@ -56,7 +56,7 @@
                             </ul>
                         </div><!--btn agente-->
                         <div class="btn-group">
-                            <button class="btn btn-mini btn-inverse dimension" objdata="">Dimensión</button>
+                            <button class="btn btn-mini btn-inverse" objdata="">Dimensión</button>
                             <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
                                 <span class="caret"></span>
                             </button>
@@ -71,69 +71,6 @@
                             <input objdata="text" class="input-mini limit" placeholder="limite">
                         </div><!--Limite-->
                     </div><!--row1-->
-=======
-                    <div class="btn-group">
-                        <button class="btn btn-mini btn-inverse type" objdata="">Gráfico</button>
-                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu grafico">
-                            <li><a objdata="line">Líneas</a></li>
-                            <li><a objdata="col">Barras</a></li>
-                            <li><a objdata="pie">Torta</a></li>
-                        </ul>
-                    </div>
-                    <div class="btn-group">
-                        <button class="btn btn-mini btn-inverse campo" objdata="">Campo</button>
-                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a objdata="sine">Agente</a></li>
-                            <li><a objdata="cia">Compañía</a></li>
-                            <li><a objdata="pcc">PCC</a></li>
-                        </ul>
-                    </div>
-                    
-                    <div class="btn-group">
-                        <button class="btn btn-mini btn-success sine" objdata="sine">Elija Agente</button>
-                        <button class="btn btn-mini btn-success dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
-                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
-                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
-                            <li><a objdata="sine" value-sabre="SB" value-amadeus="AM">luis_bernal</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="btn-group">
-                        <button class="btn btn-mini btn-inverse dimension" objdata="">Dimensión</button>
-                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a objdata="pcc">PCC</a></li>
-                            <li><a objdata="limit">Límite</a></li>
-                            <li><a objdata="gds">GDS</a></li>
-                            <li><a objdata="sine">Agente</a></li>
-                        </ul>
-                    </div>
-                    <input type="text" class="input-mini limit" placeholder="limite">
-                    <div class="btn-group">
-                        <button class="btn btn-mini btn-inverse filtro" objdata="">Filtro</button>
-                        <button class="btn btn-mini btn-inverse dropdown-toggle" data-toggle="dropdown">
-                        <span class="caret"></span>
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a objdata="month">Mes</a></li>
-                            <li><a objdata="year">Año</a></li>
-                            <li><a objdata="gds">GDS</a></li>
-                        </ul>
-                    </div>
-                    <!-- 
->>>>>>> master
                     <div class="drag">
                         <i class="icon-move" title="Arrastrar gráfico"></i>
                     </div>
@@ -167,10 +104,9 @@
                     <div>
                         <div id="piecolchart5" class="pie"></div>
                     </div>
-                    
                 </div>
                 <!--dashboard5-->
-                <div id="dashboard4" class="span6 dashboard draggable">
+                <div id="dashboard5" class="span6 dashboard draggable">
                     <div class="row1">
                         <div class="btn-group">
                             <button class="btn btn-mini btn-inverse campo" objdata="">Campo</button>
@@ -245,6 +181,7 @@
                         <div id="piecolchart4" class="pie"></div>
                     </div>
                 </div>
+
             </div><!--row-fluid-->
             <div id="dashboard1" class="span12 dashboard draggable">
                 <div class="btn-group">
